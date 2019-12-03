da5997e19e986571a9116b19050deb4432b5413c
5korpi0n <kevindarenas@gmail.com>
2019-12-02 22:55:28 -0300

    cambio a Mongo

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
 module.exports = {
-  database:'mongodb://u0rt0hn5ptfjmvifbtnn:uFQoaNjbZSX4sRc3CUpC@bolyohaadex87ge-mongodb.services.clever-cloud.com:27017/bolyohaadex87ge',
-  secret: 'uFQoaNjbZSX4sRc3CUpC'
-//  user: 'u0rt0hn5ptfjmvifbtnn'
+  database: 'mongodb+srv://triviauser:!!Kevin2002@cluster0-w5wsy.mongodb.net/test?retryWrites=true&w=majority'
 }
 /*
   database:'mongodb://localhost:27017/mitrivia',
   secret: 'clavemia'
 
+  database:'mongodb://u0rt0hn5ptfjmvifbtnn:uFQoaNjbZSX4sRc3CUpC@bolyohaadex87ge-mongodb.services.clever-cloud.com:27017/bolyohaadex87ge',
+  secret: 'uFQoaNjbZSX4sRc3CUpC'
+//  user: 'u0rt0hn5ptfjmvifbtnn'
 
   
 */
