9a4054efbc76c24907d3217ef9eb10f1358885f4
5korpi0n <kevindarenas@gmail.com>
2019-12-02 22:28:14 -0300

    conexion

--- config/database.js
+++ config/database.js
@@ -1,4 +1,10 @@
 module.exports = {
+  database:'mongodb://u0rt0hn5ptfjmvifbtnn:uFQoaNjbZSX4sRc3CUpC@bolyohaadex87ge-mongodb.services.clever-cloud.com:27017/bolyohaadex87ge',
+  secret: 'uFQoaNjbZSX4sRc3CUpC'
+//  user: 'u0rt0hn5ptfjmvifbtnn'
+}
+/*
   database:'mongodb://localhost:27017/mitrivia',
   secret: 'clavemia'
-}
+
+*/
